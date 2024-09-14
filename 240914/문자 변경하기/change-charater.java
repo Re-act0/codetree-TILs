6c826d8a16f1c5730bba74ba52dc879a443befc4
김준기 <118522372+Re-act0@users.noreply.github.com>
2024-09-14 13:02:08 +0900

    Title: 문자 변경하기, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240914/문자 변경하기/change-charater.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main { // 3226번 전화요금
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+        char a = 'C';
+        a = 'T';
+
+
+        bw.write(a);
+
+        bw.flush();
+    }
+}
