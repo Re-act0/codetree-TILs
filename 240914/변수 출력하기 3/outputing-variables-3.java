1fb338d2dc376acbe8219a8302b93bbbd1041fb3
김준기 <118522372+Re-act0@users.noreply.github.com>
2024-09-14 11:40:14 +0900

    Title: 변수 출력하기 3, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240914/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main { // 3226번 전화요금
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+        int a = 1;
+        int b = 2;
+        char c = 'C';
+
+
+        bw.write(a + "->" + b + "->" + c);
+
+        bw.flush();
+    }
+}
