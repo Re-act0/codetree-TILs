1107dbfc416421d29419b45cb8023dc973b420a4
김준기 <118522372+Re-act0@users.noreply.github.com>
2024-09-14 11:34:09 +0900

    Title: 다양하게 출력, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240914/다양하게 출력/print-in-variety.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main { // 3226번 전화요금
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+
+        bw.write("Total days in Year\n");
+        bw.write("365\n");
+        bw.write("Circumference rate\n");
+        bw.write("3.1415926535");
+        
+        bw.flush();
+    }
+}
