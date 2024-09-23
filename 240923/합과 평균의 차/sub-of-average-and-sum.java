c7389e0940b782c3ed0c1faa4c2891e0dbe608cc
김준기 <118522372+Re-act0@users.noreply.github.com>
2024-09-23 15:46:46 +0900

    Title: 합과 평균의 차, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240923/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main { // 3226번 전화요금
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int sum = a + b + c;
+        int avg = (a + b + c) / 3;
+        int res = sum - avg;
+
+        bw.write(sum + "\n" + avg + "\n" + res);
+        bw.flush();
+    }
+}
