4f957dc3167e20e31d4e488e70818ca472e34dcf
김준기 <118522372+Re-act0@users.noreply.github.com>
2024-09-26 14:32:28 +0900

    Title: 1부터 n까지 출력, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240926/1부터 n까지 출력/print-from-1-to-n.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main { // CODE_TREE
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= n; i++) {
+            bw.write(i + " ");
+        }
+        bw.flush();
+    }
+}
