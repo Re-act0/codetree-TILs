49ded3909855c25231e0880b66147ef60205fe7f
김준기 <118522372+Re-act0@users.noreply.github.com>
2024-09-27 10:42:12 +0900

    Title: 홀수만 출력 2, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240927/홀수만 출력 2/output-only-odd-2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main { // CODE_TREE
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int b = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+
+        for (int i = b; i >= a; i -= 2){
+            bw.write(i+" ");
+        }
+        bw.flush();
+    }
+}
