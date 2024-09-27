50f4e8fa1d5be5030639a3b9fc5ab31f975b0bac
김준기 <118522372+Re-act0@users.noreply.github.com>
2024-09-27 10:55:48 +0900

    Title: 별 n개 출력하기, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240927/별 n개 출력하기/print-n-stars.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main { // CODE_TREE
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int i = 0;
+        while(i < 5){
+            bw.write("*\n");
+            i++;
+        }
+
+        bw.flush();
+    }
+}
