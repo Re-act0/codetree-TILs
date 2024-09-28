e0fbc9a2a8d2fe0f194ff4fa2177167ce7826799
김준기 <118522372+Re-act0@users.noreply.github.com>
2024-09-28 20:28:15 +0900

    Title: 짝수만 출력, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240928/짝수만 출력/output-only-even.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main { // CODE_TREE
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int i = a;
+        while (i <= b) {
+            bw.write(i + " ");
+            i += 2;
+        }
+
+        bw.flush();
+    }
+}
