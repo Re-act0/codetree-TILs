b8f1d63af12dec1f140ca65a2241a0fab7ac06f6
김준기 <118522372+Re-act0@users.noreply.github.com>
2024-10-07 15:39:37 +0900

    Title: 규칙을 찾아 출력하기, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241007/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main { // CODE_TREE
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1 || i > j) {
+                    bw.write("* ");
+                }
+                else
+                    bw.write("  ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+    }
+}
